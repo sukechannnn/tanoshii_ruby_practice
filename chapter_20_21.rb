1f8b272e9f6f57ab739b0156b54f24a6896e1f5b
yosuke0315 <wonderfulworld0315@gmail.com>
2017-04-26 19:27:19 +0900

    コメント頂いた部分を修正しました

--- chapter_20_21.rb
+++ chapter_20_21.rb
@@ -13,40 +13,74 @@ module Chapter20
 
   class Exercise2
     require 'time'
+
     def self.jparsedate(jp_date)
       date = Time.strptime(jp_date, '%Y年%m月%d日午後%H時%M分%S秒')
       p date += 43_200 if jp_date =~ /午後/
     end
+
+    # def self.jparsedate(str)
+    #   now = Time.now
+    #   year = now.year
+    #   month = now.month
+    #   day = now.day
+    #   hour = now.hour
+    #   min = now.min
+    #   sec = now.sec
+    #   str.scan(/(午前|午後)?(\d+)(年|月|日|時|分|秒)/) do
+    #     case $3
+    #     when "年"
+    #       year = $2.to_i
+    #     when "月"
+    #       month = $2.to_i
+    #     when "日"
+    #       day = $2.to_i
+    #     when "時"
+    #       hour = $2.to_i
+    #       hour += 12 if $1 == "午後"
+    #     when "分"
+    #       min = $2.to_i
+    #     when "秒"
+    #       sec = $2.to_i
+    #     end
+    #   end
+    #   return Time.mktime(year, month, day, hour, min, sec)
+    # end
   end
 
   class Exercise3
     def self.ls_t(dir)
       files = []
-      Dir.open(dir) do |d|
-        d.each do |file|
-          next if file =~ /^\./
-          files << file
-        end
-      end
+      # Dir.open(dir) do |d|
+      #   d.each do |file|
+      #     next if file =~ /^\./
+      #     files << file
+      #   end
+      # end
+      files = Dir.entries(dir).reject { |file| file.start_with?('.') }
       sort_by_mtime = files.sort_by { |file| File.mtime(File.join(dir, file)) }
-      sort_by_mtime.each { |f| print f + ' ' }
+      # sort_by_mtime.each { |f| print f + ' ' }
+      puts sort_by_mtime.join(' ')
     end
   end
 
+  require 'forwardable'
   class Exercise4
+    extend Forwardable
+
+    def_delegators :@today, :year, :month
+
     def initialize
-      @month_ary = %w[January February March April May June July August September October November December]
+      @month_ary = Date::MONTHNAMES
       @today = Date.today
-      @year = @today.year
-      @month = @today.month
-      @start_at_month = Date.new(@year, @month, 1)
-      @end_of_month = Date.new(@year, @month, -1)
+      @start_at_month = Date.new(year, month, 1)
+      @end_of_month = Date.new(year, month, -1)
       @day = 1
     end
 
     def make_calendar
       p @start_at_month.wday
-      puts "    #{@month_ary[@month]} #{@year}    "
+      puts "    #{@month_ary[month]} #{year}    "
       puts 'Su Mo Tu We Th Fr Sa'
       @start_at_month.wday.times { print '   ' }
       loop do
@@ -58,8 +92,8 @@ module Chapter20
     end
 
     def make_day_str(day)
-      return "#{day} ".insert(0, ' ') if "#{day} ".length == 2
-      "#{day} "
+      # "#{day} ".length == 2 ? " #{day} " : "#{day} "
+      sprintf('%2d ', day)
     end
   end
 end
@@ -74,7 +108,6 @@ puts 'exercise 2'
 Chapter20::Exercise2.jparsedate('2015年9月25日午後8時17分50秒')
 puts 'exercise 3'
 Chapter20::Exercise3.ls_t('./')
-puts
 puts 'exercise 4'
 Chapter20::Exercise4.new.make_calendar
 puts
@@ -96,9 +129,9 @@ module Chapter21
   class Exercise2
     def self.exercise2
       to_class = :class.to_proc
-      p to_class.call('test')
-      p to_class.call(123)
-      p to_class.call(2**100)
+      p to_class.call('test')    #=> String
+      p to_class.call(123)       #=> Integer
+      p to_class.call(2**100)    #=> Integer
     end
   end
 
